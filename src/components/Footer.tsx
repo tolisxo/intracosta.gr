995ed2eeb3ed63103c930247fe713e4a3be078e8
tolisxo <tolisagiannnn@gmail.com>
2025-06-26 16:49:46 +0200

    17

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,13 +38,13 @@ const Footer: React.FC = () => {
             </div>
             <div className="flex space-x-4">
               <a href="#" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors">
-                <Facebook className="w-5 h-5" />
+                <Facebook className="w-5 h-5 text-yellow-500" />
               </a>
               <a href="#" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors">
-                <Linkedin className="w-5 h-5" />
+                <Linkedin className="w-5 h-5 text-yellow-500" />
               </a>
-              <a href="#" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-yellow-500 transition-colors">
-                <Instagram className="w-5 h-5" />
+              <a href="#" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors">
+                <Instagram className="w-5 h-5 text-yellow-500" />
               </a>
             </div>
           </div>
