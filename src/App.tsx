df127c97cbc784cd1f1d47f87880138b00b2c641
tolisagiannnn <tolisagiannnn@gmail.com>
2025-07-16 13:41:23 +0000

    15

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { LanguageProvider } from './contexts/LanguageContext';
 import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
 import TrustSection from './components/TrustSection';
 import Services from './components/Services';
@@ -11,25 +12,37 @@ import Testimonials from './components/Testimonials';
 import FAQ from './components/FAQ';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+import TermsOfServicePage from './pages/TermsOfServicePage';
 
 function App() {
   return (
     <LanguageProvider>
-      <div className="min-h-screen">
-        <Header />
-        <main>
-          <Hero />
-          <TrustSection />
-          <Services />
-          <Coverage />
-          <QuoteForm />
-          <About />
-          <Testimonials />
-          <FAQ />
-          <Contact />
-        </main>
-        <Footer />
-      </div>
+      <Router>
+        <div className="min-h-screen">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Hero />
+                  <TrustSection />
+                  <Services />
+                  <Coverage />
+                  <QuoteForm />
+                  <About />
+                  <Testimonials />
+                  <FAQ />
+                  <Contact />
+                </>
+              } />
+              <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+              <Route path="/terms-of-service" element={<TermsOfServicePage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
     </LanguageProvider>
   );
 }
