3d4666e72d88dc350fd04345df84b9ce394fe954
tolisxo <96475892+tolisxo@users.noreply.github.com>
2025-07-21 16:23:54 +0300

    Integrate smooth scroll component across the website

---
+++ src/components/ui/smooth-scroll.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { ReactLenis } from 'lenis/react';
+import React, { forwardRef, ReactNode } from 'react';
+
+interface SmoothScrollProps {
+  children: ReactNode;
+}
+
+const SmoothScroll = forwardRef<HTMLDivElement, SmoothScrollProps>(({ children }, ref) => {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothWheel: true }}>
+      <div ref={ref}>
+        {children}
+      </div>
+    </ReactLenis>
+  );
+});
+
+SmoothScroll.displayName = 'SmoothScroll';
+
+export default SmoothScroll;
