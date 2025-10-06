dbf7613cedea129ac11bda75847bb1a010f7ec1c
tolisxo <96475892+tolisxo@users.noreply.github.com>
2025-10-06 15:12:56 +0300

    Updated README.md

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
-import './i18n'; // Import the i18n configuration
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
